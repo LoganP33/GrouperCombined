a2388304ec953c1b8703aa06c8cfb3ccb5336023
Logan Prestigiacomo <loganpp33@hotmail.com>
2021-02-05 02:45:14 -0500

    Initial Commit

---
+++ Grouper/GrouperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrouperApp.swift
+//  Grouper
+//
+//  Created by Logan Prestigiacomo on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GrouperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
