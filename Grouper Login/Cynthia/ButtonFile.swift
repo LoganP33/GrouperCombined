9660979a7aa34be1cc9d9e1d0ac26a4231d60cea
Logan <70170467+LoganP33@users.noreply.github.com>
2021-04-09 16:30:56 -0400

    Add files via upload

---
+++ Grouper Login/Cynthia/ButtonFile.swift
@@ -0,0 +1,16 @@
+//
+//  ButtonFile.swift
+//  FriendPageReadytoSubmit
+//
+//  Created by Cynthianitza21 on 3/17/21.
+//
+
+import UIKit
+
+extension UIButton{
+    func roundbutton(){
+        
+        self.layer.cornerRadius=10
+        self.layer.masksToBounds = true
+    }
+}
